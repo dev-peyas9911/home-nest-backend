aa9dbab745f947525e66fa75333019adac5d70fe
Peyas Barmon <dev.peyasbarmon0024@gmail.com>
2026-04-04 21:03:24 +0600

    Mongodb connected

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
+// import express, { json } from 'express';
+// import cors from 'cors';
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin: [process.env.CLIENT_DOMAIN],
+        credentials: true,
+        optionSuccessStatus: 200,
+    }
+));
 app.use(express.json());
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+})
+async function run() {
+    try {
+        const db = client.db('homenestDB')
+
+
+        // Send a ping to confirm a successful connection
+        await client.db('admin').command({ ping: 1 })
+        console.log(
+            'Pinged your deployment. You successfully connected to MongoDB!'
+        )
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+    }
+}
+run().catch(console.dir)
+
 
 // API
 app.get('/', (req, res) => {
